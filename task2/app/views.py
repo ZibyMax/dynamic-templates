2ce05ee7f4a2b8bec97206f9dd3bbcf03eaf25c3
Ziby <max@rekstar-nn.ru>
2019-03-26 20:07:27 +0300

    добавил второе задание

---
+++ task2/app/views.py
@@ -0,0 +1,51 @@
+from django.shortcuts import render
+from django.views.generic import TemplateView
+
+
+class HomeView(TemplateView):
+    template_name = 'app/home.html'
+
+
+class AboutView(TemplateView):
+    template_name = 'app/about.html'
+
+    def get(self, request, *args, **kwargs):
+        context = {
+            'about_is_active': True
+        }
+        return render(request, self.template_name, context)
+
+
+class ContactsView(TemplateView):
+    template_name = 'app/contacts.html'
+
+    def get(self, request, *args, **kwargs):
+        context = {
+            'contacts_is_active': True
+        }
+        return render(request, self.template_name, context)
+
+
+class ExamplesView(TemplateView):
+    template_name = 'app/examples.html'
+
+    def get(self, request, *args, **kwargs):
+        items = [{
+            'title': 'Apple II',
+            'text': 'Легенда',
+            'img': 'ii.jpg'
+        }, {
+            'title': 'Macintosh',
+            'text': 'Свежие новинки октября 1983-го',
+            'img': 'mac.jpg'
+        }, {
+            'title': 'iMac',
+            'text': 'Оригинальный и прозрачный',
+            'img': 'imac.jpg'
+        }]
+        context = {
+            'items': items,
+            'examples_is_active': True
+        }
+        return render(request, self.template_name,
+                      context)
