60a488fe33366382bd6329d49dbb69b42717de88
Ziby <max@rekstar-nn.ru>
2019-04-22 19:39:49 +0300

    task1 готов

--- task1/app/views.py
+++ task1/app/views.py
@@ -1,7 +1,8 @@
 import csv
 import os
 
-from app.settings import BASE_DIR
+#from app.settings import BASE_DIR
+from django.conf import settings
 
 from django.shortcuts import render
 from django.views.generic import TemplateView
@@ -12,29 +13,12 @@ class InflationView(TemplateView):
     def get(self, request, *args, **kwargs):
         # чтение csv-файла и заполнение контекста
         context = {}
-        DATA_CSV_FILE = os.path.join(BASE_DIR, 'inflation_russia.csv')
-        context['data'] = []
+        DATA_CSV_FILE = os.path.join(settings.BASE_DIR, 'inflation_russia.csv')
 
         with open(DATA_CSV_FILE, 'r', encoding='UTF-8') as f:
             reader = csv.DictReader(f, delimiter=';')
-            for line in reader:
-                context['data'].append(line)
+            context['data'] = list(reader)
 
-        context['headers'] = [
-            'Год',
-            'Январь',
-            'Февраль',
-            'Март',
-            'Апрель',
-            'Май',
-            'Июнь',
-            'Июль',
-            'Август',
-            'Сентябрь',
-            'Октябрь',
-            'Ноябрь',
-            'Декабрь',
-            'Всего'
-        ]
+        context['headers'] = list(context['data'][0].keys())
 
         return render(request, self.template_name, context)
